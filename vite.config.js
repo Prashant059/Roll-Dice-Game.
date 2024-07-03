404997fa39d29b1548c231745584e64306e138a1
Prashant Randive <prashant.randhive05@gmail.com>
2024-07-03 14:21:43 +0530

    Edit vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Roll-Dice-Game/",
+  base: "/Roll-Dice-Game./",
   plugins: [react()],
 })
